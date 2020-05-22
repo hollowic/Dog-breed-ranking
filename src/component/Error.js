9f224a65ff2a26cafa0328aa23d41b4a10e59af3
Jay Zhou <hollowic@hotmail.com>
2020-05-22 09:41:16 -0700

    Error message properly styled

--- src/component/Error.js
+++ src/component/Error.js
@@ -13,17 +13,28 @@ const useStyles = makeStyles(() => ({
     backgroundColor: "rgba(30, 45, 90, 0.64)",
   },
   paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
     backgroundColor: "#ffffff",
     borderRadius: "10px",
-    padding: "75px 20px 20px",
+    padding: "20px",
     outline: 0,
-    width: "350px",
-    height: "420px",
+    width: "316px",
+    height: "120px",
     boxShadow: "rgba(0, 0, 0, .04) 0 0 0 1px, rgba(0, 0, 0, .1) 0 2px 10px",
     position: "relative",
-    "&.error": {
-      animation: "$shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
-    },
+    animation: "$shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+  },
+  title: {
+    fontWeight: "700",
+    fontFamily: "'Sen', sans-serif",
+  },
+  message: {
+    fontWeight: "300",
+    fontFamily: "'Raleway', sans-serif",
+    fontSize: "12px",
   },
   "@keyframes shake": {
     "10%, 90%": {
@@ -62,7 +73,12 @@ export default function Error({ open, handleCloseOnClick }) {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper}>"Woof Invalid Action Woof"</div>
+          <div className={classes.paper}>
+            <div className={classes.title}>"Woof Invalid Action Woof!"</div>
+            <div className={classes.message}>
+              "Maximum of 19 breeds and minimum of 1 per table."
+            </div>
+          </div>
         </Fade>
       </Modal>
     </div>
